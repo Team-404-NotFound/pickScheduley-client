131f918ee5ff782294412adaa1eaa98d37cd59a4
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-07-19 12:30:16 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-5xl h-screen flex justify-center items-center text-blue-500 font-serif">Hello everyone, welcome to our pickSchedule application.</h1>
+    </div>
+  );
+}
+
+export default App;
